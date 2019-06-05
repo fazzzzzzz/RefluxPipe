c98f8fc9f6aab76c198df1db1e7c7bedff4e2027
Faz <38828699+faz658026@users.noreply.github.com>
2019-06-05 23:26:04 +0800

    Update settings.py
    DEBUG = False

--- RefluxPipe/settings.py
+++ RefluxPipe/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h^44$zxpmmb!tzervcyne3pph4(ro-bj5d30xyaugd2f5#zva('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
